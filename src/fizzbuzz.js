94e6ffb2dd3126f46f911fe6dd49e4770125595b
Alhassan Kassama <alhassankassama@hotmail.com>
2024-04-24 23:51:54 +0300

    fizzbuzz complete

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,14 @@ const answer = []
 
 // Write your code below this line
 
-
-
-
-
+for (let i = 0; i <= 15; i++) {
+  if((i % 3 == 0) && (i % 5 == 0)){
+    console.log(i, 'FizzBuzz');
+  }else if (i % 3 == 0){
+    console.log(i, 'Fizz')
+  }else if (i % 5 == 0) {
+    console.log(i, 'Buzz')
+  }
+}
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
